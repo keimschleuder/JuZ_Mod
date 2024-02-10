0afc1517ab69395adb0e116ee1c83156269892f7
keimschleuder <freunde@niklas-keim.de>
2024-02-10 19:23:56 +0100

    Update RoleInfo.cs

--- JuZ_Mod/RoleInfo.cs
+++ JuZ_Mod/RoleInfo.cs
@@ -33,7 +33,7 @@ namespace TheOtherRoles
 
         public static RoleInfo alman = new RoleInfo("Eyub", Alman.color, "Place Towels in front of tasks to prevent them from being used", "Locks Tasks with his Towels", RoleId.Alman);                       // Button functionality missing
         public static RoleInfo deutscher = new RoleInfo("Jens", Deutscher.color, "Pace Towels to block other players", "Blocks paths with his towels", RoleId.Deutscher, true);
-        public static RoleInfo crewmatratze = new RoleInfo("Gina", Crewmatratze.color, "Stay with another one to not get voted out", "Stays with someone to not get voted out", RoleId.Crewmatratze);
+//        public static RoleInfo crewmatratze = new RoleInfo("Gina", Crewmatratze.color, "Stay with another one to not get voted out", "Stays with someone to not get voted out", RoleId.Crewmatratze);     Remove, change Name or ask
         public static RoleInfo influencer = new RoleInfo("David", Influencer.color, "Make selfies with the dead", "Has to make a selfie before reporting", RoleId.Influencer);
         public static RoleInfo amerikaner = new RoleInfo("Donald Trump", Amerikaner.color, "You have a bigger Kill Radius", "Can Kill from farther away", RoleId.Amerikaner);
         // public static RoleInfo amerikaner = new RoleInfo("Carsten Stahl", Amerikaner.color, "You have a bigger Kill Radius", "Can Kill from farther away", RoleId.Amerikaner);
